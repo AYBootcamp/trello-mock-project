b6c4c831a0468e075e4a40b3035b8622379f9fae
guohao.yan <sccdmaomao@hotmail.com>
2023-01-14 20:30:54 -0500

    fix eslint

--- server/src/server.js
+++ server/src/server.js
@@ -1,39 +1,39 @@
 import express from 'express'
-import bodyParser from 'body-parser';
+import bodyParser from 'body-parser'
 import cors from 'cors'
-import swaggerJsDoc from 'swagger-jsdoc';
+import swaggerJsDoc from 'swagger-jsdoc'
 import swaggerUI from 'swagger-ui-express'
 import path from 'path'
-import { fileURLToPath } from 'url';
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+import { fileURLToPath } from 'url'
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 // Configuration & Constants
 const PORT = 3000
 const swaggerOptions = {
     swaggerDefinition: {
         info: {
-            title: "Library API",
+            title: 'Library API',
             version: '1.0.0',
         },
     },
-    apis: ["src/server.js"],
-};
+    apis: ['src/server.js'],
+}
 
 const app = express()
-const swaggerDocs = swaggerJsDoc(swaggerOptions);
-let books = [{ id: '123', title: 'Harry Potter' }];
+const swaggerDocs = swaggerJsDoc(swaggerOptions)
+let books = [{ id: '123', title: 'Harry Potter' }]
 
 // Middleware
 app.use(cors())
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs));
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs))
 
 // Endpoints
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname + '/index.html'));
-});
+    res.sendFile(path.join(__dirname + '/index.html'))
+})
 
 /**
  * @swagger
@@ -43,11 +43,13 @@ app.get('/', (req, res) => {
  *     responses:
  *       200:
  *         description: Success
- * 
+ *
  */
 app.get('/books', (req, res) => {
-    res.json(books);
-});
+    res.json(books)
+})
 
 // Start the server
-app.listen(PORT, () => console.log(`Server started on port ${PORT}! \n-------------`))
+app.listen(PORT, () =>
+    console.log(`Server started on port ${PORT}! \n-------------`)
+)
