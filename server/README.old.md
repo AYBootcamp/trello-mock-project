23391599c866e9a4447e177f3397dc8c0e0495de
guohao.yan <sccdmaomao@hotmail.com>
2023-05-10 18:03:15 -0400

    add swagger ui

---
+++ server/README.old.md
@@ -0,0 +1,6 @@
+# Serverless Trello clone backend
+
+## Lambda Functions
+AWS Lambda functions will be written here
+
+## Swagger API
---
+++ server/src/SwaggerApp.tsx
@@ -0,0 +1,9 @@
+import 'swagger-ui-react/swagger-ui.css'
+
+import SwaggerUI from 'swagger-ui-react'
+
+const SwaggerApp = () => {
+    return <SwaggerUI url="https://petstore.swagger.io/v2/swagger.json" />
+}
+
+export default SwaggerApp
