95457e07c6f3443ea74e1f6d6af40a34e4fce5dd
guohao.yan <sccdmaomao@hotmail.com>
2023-05-29 23:22:43 -0400

    update create list methods to also create card order

--- lambda/list/createList.js
+++ lambda/list/createList.js
@@ -2,7 +2,8 @@ import { DynamoDBClient, PutItemCommand } from "@aws-sdk/client-dynamodb";
 import { v4 as uuidv4 } from 'uuid'
 
 const REGION = 'ca-central-1';
-const TABLE_NAME = 'trello-list'
+const LIST_TABLE_NAME = 'trello-list'
+const CARD_ORDER_TABLE_NAME = 'trello-card-order'
 
 // Create a DynamoDB document client
 const client = new DynamoDBClient({
@@ -12,6 +13,15 @@ const client = new DynamoDBClient({
 export const createList = async (title) => {
     // Generate a unique ID for the list
     const listId = uuidv4();
+
+    let res = []
+    res.push(await createNewList(listId, title))
+    res.push(await createNewCardOrder(listId))
+    return res
+}
+
+
+const createNewList = async (listId, title) => {
     // Create the DynamoDB item
     const list = {
         id: { S: listId },
@@ -20,7 +30,7 @@ export const createList = async (title) => {
 
     // Create the DynamoDB parameters for the PutItem operation
     const params = {
-        TableName: TABLE_NAME,
+        TableName: LIST_TABLE_NAME,
         Item: list
     };
 
@@ -34,6 +44,29 @@ export const createList = async (title) => {
         return { statusCode: 201, message: JSON.stringify('List created!'), data: list };
     } catch (err) {
         return { statusCode: 400, message: JSON.stringify(`Unable to create list. ${err}`) };
+    }
+}
+
+const createNewCardOrder = async (listId) => {
+    const cardOrderId = uuidv4()
+    const orderedCardIds = []
+    const cardOrder = {
+        id: { S: cardOrderId },
+        listId: { S: listId },
+        orderedCardIds: { L: orderedCardIds }
+    }
+    const params = {
+        TableName: CARD_ORDER_TABLE_NAME,
+        Item: cardOrder
+    }
+    try {
+        // Create the PutItem command
+        const command = new PutItemCommand(params);
+        // Send the command to DynamoDB
+        await client.send(command);
+        return { statusCode: 201, message: JSON.stringify('CardOrder created!'), data: cardOrder };
+    } catch (err) {
+        return { statusCode: 400, message: JSON.stringify(`Unable to create CardOrder. ${err}`) };
     }
 }
 
